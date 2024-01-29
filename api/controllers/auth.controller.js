20a67e1130a6536233ca6a5cf39c4276f66cbfe9
arvindreddy422 <arvindreddy422@gmail.com>
2024-01-29 13:52:10 +0530

    middleware for error display

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs'
 
-export const auth = async (req, res) => {
+export const auth = async (req, res, next) => {
   const { username, email, password } = req.body
   const hashPassword = bcryptjs.hashSync(password, 10)
   const newUser = new User({ username, email, password: hashPassword })
@@ -9,6 +9,6 @@ export const auth = async (req, res) => {
     await newUser.save()
     res.status(201).json({ msg: 'created user' })
   } catch (err) {
-    res.status(500).json(err.message)
+    next(err)
   }
 }
