157a15b8764841cd6765a47c62af07ae37ba563b
arvindreddy422 <arvindreddy422@gmail.com>
2024-01-29 12:11:34 +0530

    mongoDb connected

--- api/index.js
+++ api/index.js
@@ -1,7 +1,17 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
+mongoose
+  .connect(process.env.MURL)
+  .then(() => console.log('DB connect'))
+  .catch((e) => console.log(e.message))
 
 const app = express()
 
 app.listen(3000, () => {
   console.log('server run on 3000')
 })
+
+// mongodb+srv://imgofakr:<password>@mern-estate.dhimo9k.mongodb.net/?retryWrites=true&w=majority
