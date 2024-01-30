e80e08c0b2954fdde87b61daa6cd3c5b1ba4bf14
arvindreddy422 <arvindreddy422@gmail.com>
2024-01-30 11:52:39 +0530

    signIn done

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,9 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs'
+import { errorHandler } from '../utils/error.js'
+import jwt from 'jsonwebtoken'
 
-export const auth = async (req, res, next) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body
   const hashPassword = bcryptjs.hashSync(password, 10)
   const newUser = new User({ username, email, password: hashPassword })
@@ -10,5 +12,20 @@ export const auth = async (req, res, next) => {
     res.status(201).json({ msg: 'User Created' })
   } catch (err) {
     next(err)
+  }
+}
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body
+  try {
+    const validUser = await User.findOne({ email })
+    if (!validUser) return next(errorHandler(404, 'User not found!'))
+    const validPassword = bcryptjs.compareSync(password, validUser.password)
+    if (!validPassword) return next(errorHandler(401, 'Wrong credentials'))
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_TOKEN)
+    const { password: pass, ...rest } = validUser._doc
+    res.cookie('access_token', token, { httpOnly: true }).status(200).json(rest)
+  } catch (error) {
+    next(error)
   }
 }
