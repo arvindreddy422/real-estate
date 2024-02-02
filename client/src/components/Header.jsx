fc3b2f75f3b2706bf6f775f411e9c3e8ec82d08e
arvindreddy422 <arvindreddy422@gmail.com>
2024-02-02 15:47:17 +0530

    Profile Protected route

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,9 @@
 import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 export default function Header() {
+  const { currentUser } = useSelector((state) => state.user)
+
   return (
     <header className="bg-slate-200">
       <div className="flex justify-between items-center mx-auto max-w-6xl p-3">
@@ -25,7 +28,15 @@ export default function Header() {
             <li className="hidden sm:inline">About</li>
           </Link>
           <Link to="/sign-in">
-            <li>SignIn</li>
+            {currentUser ? (
+              <img
+                className="rounded-full h-7 w-7 object-cover"
+                src={currentUser.avatar}
+                alt="profile"
+              />
+            ) : (
+              <li className=" text-slate-700 hover:underline"> Sign in</li>
+            )}
           </Link>
         </ul>
       </div>
